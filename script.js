7942645b4541fc3fd8ec9b50162c6a645b8879e5
shmulik55 <gshloman@gmail.com>
2024-04-03 20:04:48 +0300

    Started implementing win feature

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 const buttonGrid = document.getElementsByClassName("button");
-let currentPlayer = "p1";
+let grid = [];
 
-for (let i = 0; i < buttonGrid.length; i++) {
-    const button = buttonGrid[i];
-    button.addEventListener("click", () => addSymbol(button));
+// Create 2d array for x's and o's
+for (let i = 0; i < 3; i++) {
+    grid[i] = [];
+    for (let j = 0; j < 3; j++) {
+        grid[i][j] = [];
+    }
 }
 
-function addSymbol(b){
+let currentPlayer = "p1";
+
+// Assigning oclick events for grid buttons
+for (let i = 0; i < buttonGrid.length; i++) {
+    const button = buttonGrid[i];
+    button.addEventListener("click", () => addSymbol(button, i), checkRow(i));
+}
+
+// Function for adding x or o to the board
+function addSymbol(b, index){
 
     const img = document.createElement("img");
 
     if(currentPlayer == "p1"){
         img.src = "x.png";
         img.className = "x";
+        grid[index][j] = 'x';
         currentPlayer = "p2";
     }
     else{
         img.src = "o.png";
         img.className = "o";
+        grid[index][j] = 'o';
         currentPlayer = "p1";
     }
 
@@ -26,4 +40,9 @@ function addSymbol(b){
     b.style.border = "none";
     b.style.backgroundColor = "transparent";
     b.disabled = true;
+    console.log(grid);
+}
+
+function checkRow(index){
+
 }
